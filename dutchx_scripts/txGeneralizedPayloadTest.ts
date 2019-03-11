16b32b5d8ebfe15aaa01498fba8d5c412c849cd8
Doug Kent <github@douglaskent.com>
2019-03-11 12:38:04 +0100

    split into separate functions

--- dutchx_scripts/txGeneralizedPayloadTest.ts
+++ dutchx_scripts/txGeneralizedPayloadTest.ts
@@ -8,18 +8,36 @@ const abiDecoder = require('abi-decoder');
 
 export const run = async (web3: Web3) => {
 
-  return sendTransactionWithReferrer(
+  const referrerAddress = '0x1bf4e7D549fD7Bf9c6BA3Be8BD2b9Af62F086220';
+
+  const txId = await sendTransactionWithReferrer(
     web3,
     'ExternalTokenLockerMock',
     // contract address
     '0x21a59654176f2689d12e828b77a783072cd26680',
     'lock',
-    // referrer address
-    '0x1bf4e7D549fD7Bf9c6BA3Be8BD2b9Af62F086220',
+    referrerAddress,
     // the usual function arguments
     [web3.toWei('1'), accounts[0]],
+    // web3 config
     { gas: 25530 }
   );
+
+  const referrerAddressFetched = await retrieveReferrerFromTransaction(
+    web3,
+    'ExternalTokenLockerMock',
+    // contract address
+    '0x21a59654176f2689d12e828b77a783072cd26680',
+    'lock',
+    txId
+  );
+
+  if (!referrerAddressFetched ||
+    (referrerAddressFetched.toLowerCase() !== referrerAddress.toLowerCase())) {
+    throw new Error('did not obtain _referrer address');
+  }
+
+  console.log('Success');
 };
 
 export const sendTransactionWithReferrer = async (
@@ -62,7 +80,19 @@ export const sendTransactionWithReferrer = async (
 
   const txId = web3.eth.sendTransaction(payload);
 
-  const tx = await web3.eth.getTransaction(txId);
+  return Promise.resolve(txId);
+};
+
+export const retrieveReferrerFromTransaction = async (
+  web3: Web3,
+  contractName: string,
+  contractAddress: Address,
+  functionName: string,
+  txId: Hash): Promise<Address> => {
+
+  const truffleContract = await Utils.requireContract(contractName);
+
+  const abi = truffleContract.abi.filter((entry: any) => entry.name === functionName)[0];
 
   abi.inputs.push({
     name: '_referrer',
@@ -71,13 +101,16 @@ export const sendTransactionWithReferrer = async (
 
   abiDecoder.addABI([abi]);
 
+  // tslint:disable-next-line: variable-name
+  const SolidityFunction = require('web3/lib/web3/function');
   const actualFunc = new SolidityFunction(
     web3.eth,
     abi,
     contractAddress
   );
 
-  const actualInput = '0x' + actualFunc.signature() + tx.input.substr(signature.length + 2);
+  const tx = await web3.eth.getTransaction(txId);
+  const actualInput = '0x' + actualFunc.signature() + tx.input.substr(10);
   const decodedData = abiDecoder.decodeMethod(actualInput);
 
   if (!decodedData.params) {
@@ -86,15 +119,5 @@ export const sendTransactionWithReferrer = async (
 
   const referrerParam = decodedData.params.filter((param: any) => param.name === '_referrer');
 
-  if (!referrerParam.length) {
-    throw new Error('did not obtain _referrer parameter');
-  }
-
-  if (referrerParam[0].value.toLowerCase() !== referrerAddress.toLowerCase()) {
-    throw new Error('did not obtain _referrer address');
-  }
-
-  console.log('Success');
-
-  return Promise.resolve(txId);
+  return Promise.resolve(referrerParam[0].value);
 };
